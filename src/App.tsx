131a7cb8fadaa8d1bc4f70bbb5bb00dd0d4aae7b
christianMiguez <kisquian@gmail.com>
2022-12-04 13:28:48 -0300

    hey hey 2

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
       <h1>Hola viteh</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          LA CUETNTA ES {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
